d3a4a0c4aa704bca695a0126ea92818a7be6a410
Paula Vaz <pcosv@cin.ufpe.br>
2019-06-06 12:42:30 -0300

    final commit

--- algorithm.py
+++ algorithm.py
@@ -39,7 +39,11 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier as RF
-import seaborn as sns; sns.set()
+import seaborn as sns;
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
+sns.set()
 from warnings import simplefilter
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -92,8 +96,11 @@ def getDistances(neighbors, y):
 def getRelativeDistances(distances):
     relativeDistances = []
     for x in distances:
-        relativeDistance = (x - min(distances)) / min(distances)
-        relativeDistances.append(relativeDistance)
+        if min(distances) == 0:
+            relativeDistances.append(0)
+        else:
+            relativeDistance = (x - min(distances)) / min(distances)
+            relativeDistances.append(relativeDistance)
     return relativeDistances
 
 
@@ -120,47 +127,30 @@ def classifyNeighbors(neighbor):
 Step 4: Estimate quality matrix 𝜷
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-def minimization(resultPredicted, trueResult, penalizingFactor):
-    n_classes = 4
+def minimization(resultPredicted, trueResult, penalizingFactor, n_classes):
 
     def f(x):
         soma = 0
         for i in range(len(resultPredicted)):
-            #print(np.reshape(x, (n_classes,n_classes)).transpose())
-            soma = soma + (penalizingFactor[i]*(distance.euclidean(np.dot(resultPredicted[i], np.reshape(x, (n_classes,n_classes)).transpose()), trueResult[i])))
+            bt = np.reshape(x, (n_classes,n_classes)).transpose()
+            rp = np.reshape(np.array(resultPredicted[i]), (n_classes, 1))
+            tr = np.reshape(np.array(trueResult[i]), (n_classes, 1))
+            soma = soma + (penalizingFactor[i]*(distance.euclidean(np.dot(bt, rp), tr)))
         #print(soma)
         return soma
 
-    # cada linha da matriz deve somar 1
-    def constraint(x):
-        dimension = np.reshape(x, (n_classes,n_classes))
-        soma = []
-        for a in range(dimension.shape[0]):
-            sump = 0.0
-            for b in range(dimension.shape[0]):
-                var = (np.reshape(x, (n_classes,n_classes)))
-                sump = sump + var[a][b]
-            soma.append(sump)
-        print('soma das linhas')
-        print(soma)
-        return soma
-
     initial_guess = np.array(np.identity(n_classes))
-
     dimension = initial_guess.shape
     bounds = [(0, 1)]*(n_classes*n_classes)
 
     eq_cons = ({'type': 'eq',
-               'fun': lambda x: np.array([x[0] + x[1] + x[2] + x[3] - 1,
-                                          x[4] + x[5] + x[6] + x[7] - 1,
-                                          x[8] + x[9] + x[10] + x[11] - 1,
-                                          x[12] + x[13] + x[14] + x[15] - 1])})
+               'fun': lambda x: np.array([x[0] + x[1] + x[2] - 1,
+                                          x[3] + x[4] + x[5] - 1,
+                                          x[6] + x[7] + x[8] - 1])})
 
-    result = optimize.minimize(f, initial_guess, method='slsqp', constraints=eq_cons, options={'disp': True})
-
-    print(result)
-
-    return np.reshape(result.x, (n_classes,n_classes))
+    result = optimize.minimize(f, initial_guess, method='slsqp', constraints=eq_cons, bounds=bounds)
+    #print(np.reshape(np.array(result.x), (n_classes,n_classes)))
+    return (np.reshape(np.array(result.x), (n_classes,n_classes)))
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -170,90 +160,125 @@ Step 5: Correct the classification result of object 𝐲
 def correctClassification(b, y, u):
     return (y*(np.dot((np.reshape(b, (len(u[0]),len(u[0]))).transpose()),(u.transpose())))) + ((1 - y)*(u.transpose()))
 
+
+
 def main():
+
+    graphResultBefore = []
+    graphResultAfter = []
 
     simplefilter(action='ignore', category=FutureWarning)
 
-    data = pandas.read_csv('kn.csv')
+    # dataset
+    data = pandas.read_csv('tae.csv')
     X = data.drop('UNS', axis=1)
     y = data['UNS']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-    gnb = GaussianNB()
-    gnb.fit(X_train, y_train)
-    # y_predVector = gnb.predict_proba(X_test)
-    # y_pred = gnb.predict(X_test)
-    # print("Accuracy:", metrics.accuracy_score(y_test, y_predVector))
-    # print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-    # mat = confusion_matrix(y_pred, y_test)
-    # print(mat)
+    classes = [1, 2, 3] # tae dataset
+    # classes = ['bus', 'opel', 'saab', 'van'] # ve dataset
+    # classes = ['High', 'Low', 'Middle', 'very_low'] # kn dataset
 
-    k = 5
-    counter = 0
-    nParameter = 5
-    for a in range(len(X_test)):
-        #print(X_test.values[a])
-        neighbors = getNeighborsKnn(X_train.values, X_test.values[a], k)
-        distances = getDistances(neighbors, X_test.values[a])
-        relativeDistances = getRelativeDistances(distances)
-        penalizingFactors = getDistancePenalizingFactors(relativeDistances, nParameter)
-        predNeighborsVector = gnb.predict_proba(neighbors)
+    yParameter = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    for i in yParameter:
+        print(i)
 
-        trueResult = []
-        for v in range(len(neighbors)):
-            for l in range(len(X_train.values)):
-                if all(neighbors[v] == X_train.values[l]):
-                    trueResult.append(y_train.values[l])
+        """""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        Escolha do algoritmo
+        """""""""""""""""""""""""""""""""""""""""""""""""""""""""
+        # Random Forest, SVM ou Naive Bayes
+        classifier = GaussianNB()
+        #classifier = RF()
+        #classifier = SVC(kernel='linear', probability=True)
 
-        trueResultVector = []
-        for true in trueResult:
-            aux = []
-            if true == 'High':
-                aux = np.zeros(4)
-                aux[0] = 1
-            if true == 'Low':
-                aux = np.zeros(4)
-                aux[1] = 1
-            if true == 'Middle':
-                aux = np.zeros(4)
-                aux[2] = 1
-            if true == 'very_low':
-                aux = np.zeros(4)
-                aux[3] = 1
-            trueResultVector.append(aux)
+        classifier.fit(X_train, y_train)
+        pred = classifier.predict(X_test)
 
-        u = gnb.predict_proba([X_test.values[a]])
-        uClass = gnb.predict([X_test.values[a]])
-        print('antes')
-        print(u)
-        print(uClass)
-        b = minimization(predNeighborsVector, trueResultVector, penalizingFactors)
-        finalResultCorrected = correctClassification(b, 0.9, u)
+        accuracyBefore = metrics.accuracy_score(y_test, pred)
+        graphResultBefore.append(accuracyBefore)
 
-        classUpdated = 0
-        classFinal = 'very_low'
-        for i in range(len(finalResultCorrected)):
-            if finalResultCorrected[i] > classUpdated:
-                classUpdated = finalResultCorrected[i]
-                if i == 0:
-                    classFinal = 'High'
-                if i == 1:
-                    classFinal = 'Low'
-                if i == 2:
-                    classFinal = 'Middle'
-        print('depois')
-        print(finalResultCorrected)
-        print(classFinal)
-        if uClass[0] != classFinal:
-            counter = counter + 1
+        print("Accuracy before optimization:", accuracyBefore)
+        matBefore = confusion_matrix(pred, y_test)
 
-    print(counter)
+        k = 5
+        counter = 0
+        nParameter = 5
+        resultCorrected = []
+        for a in range(len(X_test)):
+            neighbors = getNeighborsKnn(X_train.values, X_test.values[a], k)
+            distances = getDistances(neighbors, X_test.values[a])
+            relativeDistances = getRelativeDistances(distances)
+            penalizingFactors = getDistancePenalizingFactors(relativeDistances, nParameter)
+            predNeighborsVector = classifier.predict_proba(neighbors)
 
-    # Random Forest
-    # clf = RF()
-    # clf.fit(X_train, y_train)
-    # pred_pro = clf.predict_log_proba(X_test)
-    # print(pred_pro)
+            trueResult = []
+            for v in range(len(neighbors)):
+                for l in range(len(X_train.values)):
+                    if all(neighbors[v] == X_train.values[l]):
+                        trueResult.append(y_train.values[l])
 
+            # vetor que recebe a 'ground truth'
+            trueResultVector = []
+            for true in trueResult:
+                aux = []
+                if true == classes[0]: # bus e High
+                    aux = np.zeros(len(classes))
+                    aux[0] = 1.0
+                if true == classes[1]: # opel e Low
+                    aux = np.zeros(len(classes))
+                    aux[1] = 1.0
+                if true == classes[2]: # saab e Middle
+                    aux = np.zeros(len(classes))
+                    aux[2] = 1.0
+                # if true == classes[3]: # van e very_low
+                #     aux = np.zeros(len(classes))
+                #     aux[3] = 1.0
+                trueResultVector.append(aux)
+
+            # predição do exemplo pelo classificador
+            u = classifier.predict_proba([X_test.values[a]])
+            uClass = classifier.predict([X_test.values[a]])
+
+            # vetor que recebe as novas respostas corrigidas pelo algoritmo
+            resultCorrected.append(uClass[0])
+
+            b = minimization(predNeighborsVector, trueResultVector, penalizingFactors, len(classes))
+            finalPredMatrix = correctClassification(b, i, u)
+
+            classUpdated = 0
+            classFinal = 1
+            #classFinal = 'van'
+            for i in range(len(finalPredMatrix)):
+                if finalPredMatrix[i] > classUpdated:
+                    classUpdated = finalPredMatrix[i]
+                    if i == 0:
+                        #classFinal = 'High'
+                        #classFinal = 'bus'
+                        classFinal = 1
+                    if i == 1:
+                        #classFinal = 'Low'
+                        #classFinal = 'opel'
+                        classFinal = 2
+                    if i == 2:
+                        #classFinal = 'Middle'
+                        #classFinal = 'saab'
+                        classFinal = 3
+
+            # se a classe muda, atualiza o vetor
+            if uClass[0] != classFinal:
+                resultCorrected[a] = classFinal
+                counter = counter + 1
+
+        # recalculando acuracia depois da otimização do resultado do algoritmo puro
+        accuracyAfter = metrics.accuracy_score(y_test, resultCorrected)
+        print("Accuracy after optimization:", accuracyAfter)
+        matAfter = confusion_matrix(resultCorrected, y_test)
+
+        graphResultAfter.append(accuracyAfter)
+
+    # grafico cuja linha laranja é a acuracia resultante da aplicacao do algoritmo e o azul representa o alforitmo sem otimizacao
+    plt.plot(yParameter, graphResultBefore)
+    plt.plot(yParameter, graphResultAfter)
+    plt.show()
 
 main()
